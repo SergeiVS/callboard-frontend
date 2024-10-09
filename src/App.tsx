038605d4cdfc6aec7eb5b2e0e3d6561ca7531cad
ValeriaKornienko <valebog94@gmail.com>
2024-10-09 20:20:44 +0200

    Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-/*import { Route, Routes} from "react-router-dom"*/
+import { Route, Routes} from "react-router-dom"
 
 import Button from "./components/Button/Button"
-
 import Layout from "components/Layout/Layout"
+import Home from "pages/Home/Home"
+
+import { PagesPaths } from "components/Layout/types"
 
 function App() {
   return (
     <>
-      {/*<Layout>
+      <Layout>
         <Routes>
           <Route path={PagesPaths.HOME} element={<Home />}></Route>
         </Routes>
-      </Layout> */}
-      <Layout>
+      </Layout>
       <Button isDeleteButton={false}>Ok</Button>
       <Button disabled>DIs</Button>
       <Button isDeleteButton>Delete</Button>
-      </Layout>
     </>
   )
 }
