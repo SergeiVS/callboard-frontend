f302b75d40fb9de9d07c261b2be8ab564aafe84a
SergeiVS <fam.smirnoff@gmail.com>
2024-10-11 21:48:30 +0200

    Add RadioGoup

---
+++ src/components/radiogroup/Styles.ts
@@ -0,0 +1,6 @@
+import { RadioGroup, styled } from "@mui/material";
+import { RadioGroupProps } from "./types";
+
+export const StyledRadioGroup= styled(RadioGroup)<RadioGroupProps>`
+width:100%;
+`
