b25a57e4c78784d1c39833d48df9c4536269050c
SergeiVS <fam.smirnoff@gmail.com>
2024-10-14 20:04:41 +0200

    format code

--- src/components/SignInForm/styles.ts
+++ src/components/SignInForm/styles.ts
@@ -5,18 +5,7 @@ import { FormsCss } from "styles/CommonCss"
 
 export const StyledSignInForm = styled(FormControl)`
   ${FormsCss}
-  
-  /* display: flex; */
-  /*
-  flex-direction: column;
-  justify-content: center;
-  align-items: center; */
-  /* gap: 20px;
-  padding: 20px;
-  width: 540px;
-  height: fit-content;
-  background-color: ${colors.LIGHTBLUE};
-  border-radius: 9px; */
+
 `
 export const StyledLable = styled(FormLabel)``
 
