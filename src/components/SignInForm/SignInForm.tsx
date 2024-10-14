f5365a08052cbbc2a7623d1b6333b8114dd0761b
SergeiVS <fam.smirnoff@gmail.com>
2024-10-14 19:28:51 +0200

    commit

--- src/components/SignInForm/SignInForm.tsx
+++ src/components/SignInForm/SignInForm.tsx
@@ -1,8 +1,8 @@
 import Input from "components/Input/input"
 import Button from "components/Button/Button"
 
-import { StyledSignInForm, StyledLable, ButtonWraper } from "./styles"
-import { SignInFormProps } from "./types"
+import { StyledSignInForm, StyledLable, ButtonWraper } from "components/SignInForm/styles"
+import { SignInFormProps } from "components/SignInForm/types"
 
 function SignInForm({ onSubmit }: SignInFormProps) {
   return (
