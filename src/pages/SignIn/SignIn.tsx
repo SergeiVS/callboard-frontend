0352f0f67f0c8eebc26d498066c2485d6e80ab03
SergeiVS <fam.smirnoff@gmail.com>
2024-10-17 18:31:44 +0200

    remove FormControl tag from SignInForm, add token to local storage

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,17 +1,15 @@
 import SignInForm from "components/SignInForm/SignInForm"
 
 import { PageWrapper } from "./styles"
-import { useAppDispatch, useAppSelector } from "store/hooks"
-import { signInActions, signInSelectors } from "store/redux/SignInFormSlice"
 
 function SignIn() {
-//     const dispatch = useAppDispatch()
-// const logIn = ()=>{dispatch(signInActions.login())}
+  //     const dispatch = useAppDispatch()
+  // const logIn = ()=>{dispatch(signInActions.login())}
 
   return (
     <>
       <PageWrapper>
-        <SignInForm onSubmit={() => {}} />
+        <SignInForm />
       </PageWrapper>
     </>
   )
